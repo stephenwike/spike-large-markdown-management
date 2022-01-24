f9bf805295fad902f86ef5db484665008e49c1ae
Stephen Wike <stephen.wike@terumobct.com>
2022-01-24 12:10:40 -0700

    pain in the butt

--- ci/compile-ddd.ps1
+++ ci/compile-ddd.ps1
@@ -1,7 +1,9 @@
 Push-Location ./docs/ddd
-    dir
+    Write-Host | Get-ChildItem -Recurse -Include *.md -Name
+    Write-Host "END OF LIST"
     $dddContent = Get-Content ../ddd-template.md
     $files = Get-ChildItem -Recurse -Include *.md -Name
+    
     foreach ($file in $files)
     {
         $content = Get-Content $file -Raw
